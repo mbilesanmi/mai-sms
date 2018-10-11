0741dfe78ef54c570ff3fbcb65355715b5909584
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2018-10-11 21:14:36 +0100

    Add the get all contacts route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const contactsController = require('../controllers/contacts');
 
 module.exports = app => {
   app.route('/contacts')
+    .get(contactsController.getAll)
     .post(contactsController.create)
 
   app.route('/contacts/:contactId')
