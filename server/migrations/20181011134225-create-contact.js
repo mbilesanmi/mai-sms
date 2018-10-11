0f6f40e754a50b282d5883f82ea761c0c7a2060a
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2018-10-11 15:18:04 +0100

    Setup the Contact model

---
+++ server/migrations/20181011134225-create-contact.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      phoneNum: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Contacts');
+  }
+};
