913f9aa9ca4f16108d5ba33ee200a8a85abda780
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2018-10-11 11:47:20 +0100

    Fix error in config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME_DEV§,
-    "password": process.env.DB_PASSWORD_DEV§,
-    "database": process.env.DB_DATABASE_DEV§,
-    "host": process.env.DB_HOST_DEV§,
+    "username": process.env.DB_USERNAME_DEV,
+    "password": process.env.DB_PASSWORD_DEV,
+    "database": process.env.DB_DATABASE_DEV,
+    "host": process.env.DB_HOST_DEV,
     "dialect": "postgres",
     "logging": false
   },
